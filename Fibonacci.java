855982b5f80487bde3620a0d6c3333cf8c058ab1
imamtri <imamtelkomuniversity@gmail.com>
2016-03-31 10:41:12 +0700

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -9,15 +9,8 @@ public class Fibonacci {
         System.out.println("Masukkan Bilangan : ");
         int number = new Scanner(System.in).nextInt();
      
-        for(int i=1; i<=number; i++){
-            System.out.print(fibonacci(i) +" ");
-        }
+        System.out.println(number+1);
     } 
  
-    public static int fibonacci(int number){
-        if(number == 1 || number == 2){
-            return 1;
-        }
-        return fibonacci(number-1) + fibonacci(number -2);
-    }
+
 }
